f6bc16e3483ff875cd9845a39097bda265e833a4
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2020-03-10 20:46:55 -0400

    Initial Commit

---
+++ CollectionViewScroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewScroll
+//
+//  Created by Neftali Samarey on 3/10/20.
+//  Copyright © 2020 Neftali Samarey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
